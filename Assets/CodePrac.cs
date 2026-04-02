5c0471d4d0e694227456de12c440e527d0f636a2
Jade <jade.kuehnel2008@gmail.com>
2026-04-01 18:18:43 -0700

    dfg

--- Assets/CodePrac.cs
+++ Assets/CodePrac.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CodePrac : MonoBehaviour
 {
+
+        public int input = 5;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        solution();
     }
 
     // Update is called once per frame
-    void Update()
+    void solution()
     {
-        
+        int answer = input * 60;
+
+        Debug.Log(input + " minute is " + answer + " seconds"); 
     }
 }
---
+++ Assets/Scripts/AttackPrefabScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AttackPrefabScript : MonoBehaviour
+{
+    public float life = 3;
+    // public HealthBar healthBar; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // healthBar = GameObject.Find("HealthBar").GetComponent<HealthBar>();
+        Destroy(gameObject, life);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //gunShoot.enemyDestroyed++;
+                // double updatedHealth = healthBar.healthBarSprite.fillAmount - 0.2;
+            healthBar.UpdateHealthBar(updatedHealth);
+            //Destroy(collision.gameObject);
+            //Destroy(gameObject);
+        }
+    }
+
+}
